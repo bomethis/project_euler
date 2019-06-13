37f9ed60628a5ce62c4025b6ccec2db1a016a905
Bebo <joelbebo@gmail.com>
2019-06-13 15:28:08 -0400

    my submission

--- 145_project_euler.py
+++ 145_project_euler.py
@@ -1,19 +1,20 @@
 
 
 def find_reverse():
-    count_num = 0
+    reversible_num_count = 0
     for i in range(1000):
         str_num = str(i)
         str_rev = str(i)[:: -1]
         if str_num[0] != "0" and str_rev[0] != "0":
-            if (int(str_num) + int(str_rev)) % 2 != 0:
-                count_num += 1
-            else:
-                i += 1
-        else:
-            i += 1
+            sum_num = int(str_num) + int(str_rev)
+            sum_num_count = 0
+            for j in range(len(str(sum_num))):
+                if int(str(sum_num)[j]) % 2 != 0:
+                    sum_num_count += 1
+            if len(str(sum_num) == sum_num_count:
+                reversible_num_count += 1
 
-    return count_num
+    return reversible_num_count
 
 
 print(find_reverse())
