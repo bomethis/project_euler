a876ee9e8a3d91f35092baa178a52a7975bd80db
Joel Bebo <joelbebo@gmail.com>
2019-06-13 17:09:04 -0400

    my solution

--- 145_project_euler.py
+++ 145_project_euler.py
@@ -1,6 +1,4 @@
 
-# Brute force method
-
 
 def find_reverse():
     reversible_num_count = 0
@@ -10,17 +8,20 @@ def find_reverse():
         # reverse number
         str_rev = str(i)[:: -1]
 
-        # check for 0's
+        # check for 0's on either end of the number
         if str_num[0] != "0" and str_rev[0] != "0":
+
             sum_num = int(str_num) + int(str_rev)
             sum_num_count = 0
 
-            # check if addition contains all odd numbers
+            # check if sum_sum contains ALL odd digits, otherwise break
             for j in range(len(str(sum_num))):
                 if int(str(sum_num)[j]) % 2 != 0:
                     sum_num_count += 1
+                else:
+                    break
 
-            # sum reversible numbers
+            # if reversible number, increment
             if len(str(sum_num)) == sum_num_count:
                 reversible_num_count += 1
 
